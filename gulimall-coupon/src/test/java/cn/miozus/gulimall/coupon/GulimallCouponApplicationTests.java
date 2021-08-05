2e0eca46030fe16cdd0386c4f81737d80f29e038
amazm-win10 <lss811@outlook.com>
2021-08-05 18:47:21 +0800

    初级项目环境构建: Hello World, via Spring Cloud

---
+++ gulimall-coupon/src/test/java/cn/miozus/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.miozus.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

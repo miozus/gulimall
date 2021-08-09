78f333b4a3bd77a6740b05ba4c9707a2805d0b16
amazm-win10 <lss811@outlook.com>
2021-08-09 21:43:24 +0800

    Openfeign 远程调用（微服务通信）:   * 所有微服务都上线了 Discovery   * 测试发起者通过注册中心，查找响应者的优惠券服务 ✅   * 修复注册中心配置，服务名称拼写错误

---
+++ gulimall-member/src/main/java/cn/miozus/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,14 @@
+package cn.miozus.gulimall.member.feign;
+
+import cn.miozus.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 以你之名，声明式远程调用请求
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+    // 路径：（模块名/实体类） + 方法（方法名/返回类型）
+    @RequestMapping("coupon/coupon/member/list")
+    public R memberCoupons();
+
+}

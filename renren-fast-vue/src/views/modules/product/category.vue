bb00ad58470e5fa6f17d360dc72af7d909c4d55c
amazm-win10 <lss811@outlook.com>
2021-08-22 22:03:57 +0800

    vue: category:    一、接口联调，网页显示菜单数据（查）    二、实现网页菜单的增删改

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -17,49 +17,40 @@
             v-if="node.level < 3"
             type="text"
             size="mini"
-            @click="() => append(data)"
+            @click="() => save(data)"
           >
             增
           </el-button>
-          
-          <el-button
-            type="text"
-            size="mini"
-            @click="() => update(node, data)"
-          >
+
+          <el-button type="text" size="mini" @click="() => update(data)">
             改
           </el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
             size="mini"
-            @click="() => remove(node, data)"
+            @click="() => delete (node, data)"
           >
             删
           </el-button>
         </span>
       </span>
     </el-tree>
-    <el-dialog title="修改" :visible.sync="dialogVisible" width="30%">
-      <el-form :model="category">
-        <el-form-item label="分类名称修改为">
-          <el-input v-model="category.name" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="updateCategory()">确 定</el-button>
-      </span>
-    </el-dialog>
-    <el-dialog title="添加" :visible.sync="dialogVisible" width="30%">
+    <el-dialog :title="dialogTitle" :visible.sync="dialogVisible" width="30%" :close-on-click-modal="false">
       <el-form :model="category">
         <el-form-item label="分类名称">
-          <el-input v-model="category.name" autocomplete="off"></el-input>
+          <el-input v-model="category.name" autocomplete="on"></el-input>
+        </el-form-item>
+        <el-form-item label="分类图标">
+          <el-input v-model="category.icon" autocomplete="on"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="on"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory()">确 定</el-button>
+        <el-button type="primary" @click="submitData()">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -68,12 +59,17 @@
 export default {
   data() {
     return {
+      dialogTitle: "",
+      dialogType: "",
       category: {
         name: "",
         parentCid: 0,
         catLevel: 0,
         showStatus: 1,
-        sort: 0
+        sort: 0,
+        productUnit: "",
+        icon: "",
+        catId: null
       },
       dialogVisible: false,
       menus: [],
@@ -85,6 +81,18 @@ export default {
     };
   },
   methods: {
+    submitData() {
+      switch (this.dialogType) {
+        case "save":
+          this.saveCategory();
+          break;
+        case "update":
+          this.updateCategory();
+          break;
+        default:
+          break;
+      }
+    },
     handleNodeClick(data) {
       console.log(data);
     },
@@ -97,28 +105,37 @@ export default {
         this.menus = data.data;
       });
     },
-    append(data) {
+    save(data) {
       console.log(data);
+      this.dialogType = "save";
+      this.dialogTitle = "新增";
       this.dialogVisible = true;
       this.category.parentCid = data.catId;
-      this.category.catLevel = data.catLevel == 3 ? 3 : data.catLevel * 1 + 1;
-      this.category.sort = data.sort;
+      this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.name = "";
+      this.category.icon = "";
+      this.category.productUnit = "";
     },
     update(data) {
-      console.log(data);
+      console.log("update:", data);
+      this.dialogType = "update";
+      this.dialogTitle = "修改";
       this.dialogVisible = true;
-      this.category.parentCid = data.catId;
-      this.category.catLevel = data.catLevel == 3 ? 3 : data.catLevel * 1 + 1;
-      this.category.sort = data.sort;
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get"
+      }).then(({ data }) => {
+        this.category = { ...data.data };
+      });
     },
     // 修改三级分类
     updateCategory() {
       console.log("修改三级绑定数据：", this.category);
+      var { catId, name, icon, productUnit } = this.category;
       this.$http({
-        // url: this.$http.adornUrl("/product/category/update"),
-        url: this.$http.adornUrl("/"),
+        url: this.$http.adornUrl("/product/category/update"),
         method: "post",
-        data: this.$http.adornData(this.category, false)
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false)
       }).then(({ data }) => {
         this.$message({
           message: "修改成功",
@@ -130,8 +147,9 @@ export default {
       });
     },
     // 添加三级分类
-    addCategory() {
+    saveCategory() {
       console.log("添加三级绑定数据：", this.category);
+
       this.$http({
         url: this.$http.adornUrl("/product/category/save"),
         method: "post",
@@ -147,7 +165,7 @@ export default {
       });
     },
 
-    remove(node, data) {
+    delete(node, data) {
       var ids = [data.catId];
       this.$confirm(`是否删除【${data.name}】菜单？`, "提示", {
         confirmButtonText: "确定",

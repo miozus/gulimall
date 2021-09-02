b74e98297011849fca495180dd2c6c0236f590a2
amazm-win10 <lss811@outlook.com>
2021-09-02 22:37:38 +0800

    vue: brand, gulimall-plugins   四、逆向工程，快速生成     1）拖入模板文件，跑通，测试增删     2）细节优化：显示状态用切换按钮表示     3）文件上传功能 OSS (开通后端获取令牌、微服务）     * 4）前端页面联调，签名后上传图片     * 5) 表单校验&自定义校验（前端校验；后端JSP303校验，集中处理异常的类，统一错误码，分组校验，自定义校验）

---
+++ gulimall-common/src/main/java/cn/miozus/common/valid/ListValueConstraintValidator.java
@@ -0,0 +1,33 @@
+package cn.miozus.common.valid;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListValueConstraintValidator implements ConstraintValidator<ListValue,Integer> {
+
+    private Set<Integer> set = new HashSet<>();
+
+    // 初始化，将详细信息给出
+    @Override
+    public void initialize(ListValue constraintAnnotation) {
+        int[] vals = constraintAnnotation.vals();
+        // TODO: 最好做非空判断，避免无数据
+        for (int val : vals) {
+            set.add(val);
+        }
+    }
+
+    /**
+     * 是有效的
+     *
+     * @param integer                    整数, 需要校验的值
+     * @param constraintValidatorContext 约束验证器上下文
+     * @return boolean
+     */// 是否校验成功
+    @Override
+    public boolean isValid(Integer integer, ConstraintValidatorContext constraintValidatorContext) {
+        return set.contains(integer);
+    }
+}
---
+++ gulimall-common/src/main/java/cn/miozus/common/valid/UpdateGroup.java
@@ -0,0 +1,4 @@
+package cn.miozus.common.valid;
+
+public interface UpdateGroup {
+}

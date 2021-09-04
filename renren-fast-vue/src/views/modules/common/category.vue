694744257bafa662c59ba52b56ae69bc564497a2
amazm-win10 <lss811@outlook.com>
2021-09-04 21:22:01 +0800

    五、商品信息业务逻辑 1）平台属性，三级维护   * 导入菜单数据（持久化数据库）：sys_menus.sql   * 前端组件抽取&父子组件交互   * 后端接口实现多条件SQL查询

---
+++ renren-fast-vue/src/views/modules/common/category.vue
@@ -0,0 +1,66 @@
+<!-- 三级分类模板 -->
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    node-key="catId"
+    ref="tree"
+    @node-click="handleNodeClick"
+  ></el-tree>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        console.log("success get data... ", data.data);
+        this.menus = data.data;
+      });
+    },
+    handleNodeClick(data, node, component) {
+      console.log("子组件被点击：", data, node, component);
+      // 向父组件发送事件
+      this.$emit("tree-node-click", data, node, component)
+    }
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    this.getMenus();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped></style>

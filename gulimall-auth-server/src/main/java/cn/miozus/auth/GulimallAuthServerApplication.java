8fe49996d2c1636acff82359b2ed374bd5058cfd
amazm-win10 <lss811@outlook.com>
2021-12-28 22:03:57 +0800

    七、认证服务   * 环境搭建   * 验证码：倒计时计时器，加锁和递归调用递减，操作 css 属性   * 简单的自动控制器：添加视图映射控制器（空方法路由）   * 发送手机短信验证码：第三方服务

---
+++ gulimall-auth-server/src/main/java/cn/miozus/auth/GulimallAuthServerApplication.java
@@ -0,0 +1,23 @@
+package cn.miozus.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * gulimall身份验证服务器应用程序
+ *
+ * @author miao
+ * @date 2021/12/28
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GulimallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallAuthServerApplication.class, args);
+    }
+
+}

f08a9edc1a8c5400070c31931a311db84ac51b8c
miozus <miozus@outlook.com>
2022-03-11 18:25:03 +0800

    clean key

---
+++ gulimall-plugins/src/test/java/cn/miozus/gulimall/plugins/GulimallPluginsApplicationTests.java
@@ -0,0 +1,145 @@
+package cn.miozus.gulimall.plugins;
+
+import cn.miozus.gulimall.plugins.component.SmsComponent;
+import cn.miozus.gulimall.plugins.controller.OssController;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+class GulimallPluginsApplicationTests {
+
+    OssController ossController;
+
+    @Autowired
+    private OSSClient ossClient;
+
+    @Autowired
+    SmsComponent smsComponent;
+
+
+    @Test
+    public void testSendSms(){
+        smsComponent.sendSMS("","131420");
+    }
+
+    @Test
+    public void sendSMS() {
+        String host = "https://fesms.market.alicloudapi.com";// 【1】请求地址 支持http 和 https 及 WEBSOCKET
+        String path = "/sms/";// 【2】后缀
+        String appcode = ""; // 【3】开通服务后 买家中心-查看AppCode
+        String code = ""; // 【4】请求参数，详见文档描述
+        String phone = ""; // 【4】请求参数，详见文档描述
+        String sign = "1"; // 【4】请求参数，详见文档描述
+        String skin = "13"; // 【4】请求参数，详见文档描述
+        String urlSend = host + path + "?code=" + code + "&phone=" + phone + "&sign=" + sign + "&skin=" + skin; // 【5】拼接请求链接
+        try {
+            URL url = new URL(urlSend);
+            HttpURLConnection httpURLCon = (HttpURLConnection) url.openConnection();
+            httpURLCon.setRequestProperty("Authorization", "APPCODE " + appcode);// 格式Authorization:APPCODE
+            // (中间是英文空格)
+            int httpCode = httpURLCon.getResponseCode();
+            if (httpCode == 200) {
+                String json = read(httpURLCon.getInputStream());
+                System.out.println("正常请求计费(其他均不计费)");
+                System.out.println("获取返回的json:");
+                System.out.print(json);
+            } else {
+                Map<String, List<String>> map = httpURLCon.getHeaderFields();
+                String error = map.get("X-Ca-Error-Message").get(0);
+                if (httpCode == 400 && error.equals("Invalid AppCode `not exists`")) {
+                    System.out.println("AppCode错误 ");
+                } else if (httpCode == 400 && error.equals("Invalid Url")) {
+                    System.out.println("请求的 Method、Path 或者环境错误");
+                } else if (httpCode == 400 && error.equals("Invalid Param Location")) {
+                    System.out.println("参数错误");
+                } else if (httpCode == 403 && error.equals("Unauthorized")) {
+                    System.out.println("服务未被授权（或URL和Path不正确）");
+                } else if (httpCode == 403 && error.equals("Quota Exhausted")) {
+                    System.out.println("套餐包次数用完 ");
+                } else {
+                    System.out.println("参数名错误 或 其他错误");
+                    System.out.println(error);
+                }
+            }
+
+        } catch (MalformedURLException e) {
+            System.out.println("URL格式错误");
+        } catch (UnknownHostException e) {
+            System.out.println("URL地址错误");
+        } catch (Exception e) {
+            // 打开注释查看详细报错异常信息
+             e.printStackTrace();
+        }
+    }
+
+    /*
+     * 读取返回结果
+     */
+    private static String read(InputStream is) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            line = new String(line.getBytes(), "utf-8");
+            sb.append(line);
+        }
+        br.close();
+        return sb.toString();
+    }
+
+    @Test
+    public void printVal() {
+        //System.out.println("ossController.endpoint = " + ossController.endpoint);
+    }
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-chengdu.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "";
+        String accessKeySecret = "";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\lss81\\Pictures\\信息中心小助手界面.png");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-miozus", "product/picture2.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+    @Test
+    public void testCloudUpload() throws FileNotFoundException {
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\lss81\\Pictures\\program_icon.png");
+
+t        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-miozus", "product/program_icon_from_plugin.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
